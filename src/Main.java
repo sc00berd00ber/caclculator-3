4dc181353f15971d4f88906694186bd9b61b1e94
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-11 14:30:14 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        NumberFormat.getCurrencyInstance();
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Enter Monthly Payout (no commas) ($): ");
+        double monthlyPayout = input.nextDouble();
+
+        System.out.println("Enter Expected Interest Rate (e.x 7.65): ");
+        double interestRate = input.nextDouble() / 100;
+
+        System.out.println("Enter Years to Pay Out: ");
+        int years = input.nextInt();
+
+        double monthlyInterestRate = interestRate / 12;
+        int months = years * 12;
+
+        double pv = monthlyPayout * (1 - Math.pow(1 + monthlyInterestRate, -months)) / monthlyInterestRate;
+
+        System.out.printf("Present Value: %.2f \n", pv);
+
     }
 }
